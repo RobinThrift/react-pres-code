f77b252a042315d7641c21d976c7de406a277e2c
Robin Thrift <thrift.d.robin@googlemail.com>
2015-05-27 17:46:01 +0200

    mutating input example

--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -2,9 +2,17 @@
 var React = require('react');
 
 class InputTest extends React.Component {
-    render() {
-        return (<input type="text" value="Hello" />);
+    constructor(props) {
+        super(props);
+        this.state = {val: 'Hello'};
     }
+    render() {                                                         
+        return (<input type="text" value={this.state.val} 
+                onChange={this._handleChange.bind(this)} />);              
+    }                                                                  
+    _handleChange(event) {                                                  
+        this.setState({val: event.target.value});                   
+    }                                                                  
 }
 
 React.render(
